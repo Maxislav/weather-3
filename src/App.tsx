311c1b55ffc558ef2ac75584e5529c2aceb3a501
Maksym_Lipatov <maximmalyshyn@gmail.com>
2023-09-15 16:59:12 +0300

    styling start

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import './App.css';
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/browser.routes";
+import styles from './App.less'
 
-
+console.log(styles['App-header'])
 
 export function Fallback() {
     return <p>Performing initial data load</p>;
 }
 
 
-export  function App() {
-    return <div>
+export function App() {
+    return <div className={ styles.App }>
         app component
-        <RouterProvider router={router} fallbackElement={<Fallback />} />
+        <RouterProvider router={ router } fallbackElement={ <Fallback/> }/>
     </div>;
 }
