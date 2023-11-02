c761b050a003a5ef0748bc7d9c5219a7cc4c3b92
Maksym_Lipatov <maximmalyshyn@gmail.com>
2023-11-02 14:55:58 +0200

    add super prod configuration

--- webpack-prod.config.ts
+++ webpack-prod.config.ts
@@ -1,1 +1,1 @@
-import merge from "webpack-merge";const common = require('./webpack.config');module.exports = merge(common, {    mode: 'production',    devtool: false,});
+import merge from "webpack-merge";const common = require('./webpack.config');module.exports = merge(common, {    mode: 'production',    devtool: false,    output: {        filename: '[name].js',        path: __dirname + '/build',        chunkFilename: '[id].js',        clean: true        /*path: path.resolve(__dirname, 'dist'),        filename: 'bundle.js'*/    },});
--- webpack-super-prod.config.js
+++ webpack-super-prod.config.js
@@ -4,17 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const webpack_merge_1 = __importDefault(require("webpack-merge"));
-const common = require('./webpack.config');
+const common = require('./webpack-prod.config');
 module.exports = (0, webpack_merge_1.default)(common, {
     mode: 'production',
     devtool: false,
     output: {
-        filename: '[name].js',
+        filename: '[name].[chunkhash].js',
         path: __dirname + '/build',
-        chunkFilename: '[id].js',
+        chunkFilename: '[id].[chunkhash].js',
         clean: true
         /*path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'*/
     },
 });
-//# sourceMappingURL=webpack-prod.config.js.map
+//# sourceMappingURL=webpack-super-prod.config.js.map
