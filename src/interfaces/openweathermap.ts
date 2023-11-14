ca83efb1c0ed1512b931842782c6ee371217977f
Lipatov <maksym_lipatov@epam.com>
2023-11-14 20:46:17 +0200

    tech check components

--- src/interfaces/openweathermap.ts
+++ src/interfaces/openweathermap.ts
@@ -1,1 +1,1 @@
-export interface OpenweathermapItem {    clouds: {        all: number    },    dt: number,    main: {        temp: number,        humidity: number    }}export interface OpenweathermapDay{    dt: number,    data: OpenweathermapItem}
+export interface OpenweathermapItem {    clouds: {        all: number    },    dt: number,    dt_txt: string,    main: {        temp: number,        humidity: number    }}export interface OpenweathermapDay{    dt: number,    data: OpenweathermapItem}
