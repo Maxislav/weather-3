9933df535a11e87bcd9ae3871628cee668af13de
maxim <maximmalyshyn@gmail.com>
2023-12-18 04:19:35 +0200

    hoc components

--- src/components/tech-check/higher-order/higher-order.tsx
+++ src/components/tech-check/higher-order/higher-order.tsx
@@ -1,6 +1,6 @@
 import React, {memo, useState} from "react";
 
-interface MyDataInterface{
+type MyDataInterface ={
     count: number,
     increase: () => void
 }
@@ -17,19 +17,21 @@ const InnerComponent = ({count, increase}: MyDataInterface): React.JSX.Element =
     </>
 }
 
-function LogProps(props: MyDataInterface) {
-    return<>
-        <div>LogProps component</div>
-        <InnerComponent {...props}></InnerComponent>
-    </>
-}
-LogProps.displayName = "LogProps(InnerComponent)"
-const Dd = memo(LogProps, (p1: MyDataInterface, p2: MyDataInterface) => {
-    if(p2.count === 5){
-        return true
+function logProps(OriginalComponent: any, callback: () => boolean) {
+    function Aa(props: MyDataInterface){
+        callback()
+        return  <>
+            <div>LogProps component</div>
+            <OriginalComponent {...props}></OriginalComponent>
+        </>
     }
+    Aa.displayName = "LogProps(InnerComponent)"
+    return Aa
+}
+const LogProps = logProps(InnerComponent, () => {
     return false
 })
+
 export const HigherOrderComponent = () => {
     const [count, setCount] = useState(0)
 
@@ -38,16 +40,16 @@ export const HigherOrderComponent = () => {
         <div>
             <h4>HigherOrderComponent</h4>
             <InnerComponent count={count} increase={() => setCount(count+1)}/>
-            <LogProps count={count} increase={() => setCount(count+1)}></LogProps>
-            <Dd count={count} increase={() => setCount(count+1)}></Dd>
+
+            <LogProps count={count} increase={() => setCount(count+1)}/>
         </div>
         <div>
             <h4> src</h4>
             <div className={'pre-wrap'}>
                 {
                     `
-                 
-interface MyDataInterface{
+
+type MyDataInterface ={
     count: number,
     increase: () => void
 }
@@ -64,31 +66,17 @@ const InnerComponent = ({count, increase}: MyDataInterface): React.JSX.Element =
     </>
 }
 
-function LogProps(props: MyDataInterface) {
-    return<>
-        <div>LogProps component</div>
-        <InnerComponent {...props}></InnerComponent>
-    </>
-}
-LogProps.displayName = "LogProps(InnerComponent)"
-const Dd = memo(LogProps, (p1: MyDataInterface, p2: MyDataInterface) => {
-    if(p2.count === 5){
-        return true
+function logProps(OriginalComponent: any) {
+    function Aa(props: any){
+        return  <>
+            <div>LogProps component</div>
+            <OriginalComponent {...props}></OriginalComponent>
+        </>
     }
-    return false
-})
-export const HigherOrderComponent = () => {
-    const [count, setCount] = useState(0)
-
-    return <>
-        <div>
-            <h4>HigherOrderComponent</h4>
-            <InnerComponent count={count} increase={() => setCount(count+1)}/>
-            <LogProps count={count} increase={() => setCount(count+1)}></LogProps>
-            <Dd count={count} increase={() => setCount(count+1)}></Dd>
-        </div>
-    </>
-}   
+    Aa.displayName = "LogProps(InnerComponent)"
+    return Aa
+}
+const LogProps = logProps(InnerComponent)
                     
                     `
                 }
