4e0ecfc12cfa1ca19e5750bcce85e6738bd8ce09
Lipatov <maksym_lipatov@epam.com>
2024-01-08 21:21:49 +0200

    useTransition hook

--- src/components/tech-check/higher-order/higher-order.tsx
+++ src/components/tech-check/higher-order/higher-order.tsx
@@ -1,4 +1,4 @@
-import React, {memo, useCallback, useEffect, useMemo, useRef, useState} from "react";
+import React, { useState} from "react";
 
 type MyDataInterface ={
     count: number,
@@ -38,14 +38,14 @@ function logProps(OriginalComponent: any, shouldUpdate: (a: any, b: any) => bool
     Aa.displayName = "LogProps(InnerComponent)"
     return Aa
 }
-const LogProps = logProps(InnerComponent, (pi, p2) => {
-    console.log(pi, p2)
+const LogProps = logProps(InnerComponent, (p1, p2) => {
+    console.log(p1, p2)
     return false
 })
 
-export const HigherOrderComponent = () => {
-    const [count, setCount] = useState(0)
 
+const SomeComponent = () => {
+    const [count, setCount] = useState(0)
     return <>
 
         <div>
@@ -53,6 +53,16 @@ export const HigherOrderComponent = () => {
             <InnerComponent count={count} increase={() => setCount(count+1)}/>
 
             <LogProps count={count} increase={() => setCount(count+1)}/>
+        </div>
+
+    </>
+}
+
+export const HigherOrderComponent = () => {
+    return <>
+
+        <div>
+           <SomeComponent/>
         </div>
         <div>
             <h4> src</h4>
@@ -64,6 +74,23 @@ type MyDataInterface ={
     count: number,
     increase: () => void
 }
+
+
+const SomeComponent = () => {
+    const [count, setCount] = useState(0)
+    return <>
+
+        <div>
+            <h4>HigherOrderComponent</h4>
+            <InnerComponent count={count} increase={() => setCount(count+1)}/>
+            
+             Higher Order Component
+            <LogProps count={count} increase={() => setCount(count+1)}/>
+        </div>
+    
+    </>
+}     
+
 const InnerComponent = ({count, increase}: MyDataInterface): React.JSX.Element => {
     return <>
         <div>
@@ -77,18 +104,32 @@ const InnerComponent = ({count, increase}: MyDataInterface): React.JSX.Element =
     </>
 }
 
-function logProps(OriginalComponent: any) {
-    function Aa(props: any){
-        return  <>
-            <div>LogProps component</div>
+function logProps(OriginalComponent: any, shouldUpdate: (a: any, b: any) => boolean) {
+
+    let prevNumber: number = null;
+    let prevResult: React.JSX.Element | undefined;
+    function Aa(props: MyDataInterface){
+        if(prevResult!==undefined && !shouldUpdate(prevNumber, props.count)){
+            return prevResult
+        }
+
+        prevNumber = props.count
+        prevResult =  <>
+            <div>LogProps component </div>
             <OriginalComponent {...props}></OriginalComponent>
         </>
+
+
+        return prevResult
     }
     Aa.displayName = "LogProps(InnerComponent)"
     return Aa
 }
-const LogProps = logProps(InnerComponent)
-                    
+const LogProps = logProps(InnerComponent, (pi, p2) => {
+    return false
+})
+
+              
                     `
                 }
             </div>
